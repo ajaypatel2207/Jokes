6e75ec407f8d54eb8900e5ae1293e2d07ffdc65c
ajaypatel2207 <ajayvaddoriya22@gmail.com>
2025-01-25 11:19:58 +0530

    Add files via upload

---
+++ Jokes/Jokes.py
@@ -0,0 +1,9 @@
+# import pip "pyjokes" for jokes 
+
+import pyjokes
+
+print("Feeling down? Maybe a good laugh is all you need. Let me tell you a joke!")
+
+jokes = pyjokes.get_joke()
+
+print(jokes)
